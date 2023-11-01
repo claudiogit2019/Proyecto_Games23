ef9d07ea223aa34403dbbdcb56c1304519562591
claudiogit2019 <claudiounju2012@gmail.com>
2023-11-01 20:09:08 -0300

    prueba3

--- fastapi/main.py
+++ fastapi/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 @app.get('/playtimeGenre/{genero}')
 
 def playtimeGenre(genero: str):
-    # Filtra los datos de género específico
+        # Filtro
     datos_genero = generos_contenido[generos_contenido['Genero'] == genero]
     
     if datos_genero.empty:
@@ -23,10 +23,10 @@ def playtimeGenre(genero: str):
    
     datos_completos = usuarios_tiempo_juego.merge(datos_genero, left_on='item_id', right_on='Id')
 
-    # Agrupa los datos por año y suma el tiempo de juego total
+        # Agrupar
     datos_agrupados = datos_completos.groupby('Lanzamiento')['tiempo_Juego_total'].sum().reset_index()
 
-    # Encuentra el año con más horas jugadas
+        # Busqueda
     max_hours_year = datos_agrupados[datos_agrupados['tiempo_Juego_total'] == datos_agrupados['tiempo_Juego_total'].max()]
 
     if not max_hours_year.empty:
@@ -39,28 +39,28 @@ def playtimeGenre(genero: str):
 
 @app.get('/userforgenre/{genero}')
 def userforgenre(genero: str):
-    # Filtra los datos de género específico
+        # Filtro
     datos_genero = generos_contenido[generos_contenido['Genero'] == genero]
 
     if datos_genero.empty:
         return {"Mensaje": "Género no encontrado"}
 
-    # Combina los datos de usuarios y tiempo de juego con los datos de género
+        # Combinacion
     
     datos_completos = usuarios_tiempo_juego.merge(datos_genero, left_on='item_id', right_on='Id')
 
     if datos_completos.empty:
         return {"Mensaje": "No se encontraron datos de tiempo de juego para el género especificado"}
 
-    # Agrupa los datos por usuario y año y suma el tiempo de juego total
+        # Agrupar
     datos_agrupados = datos_completos.groupby(['user_id', 'Lanzamiento'])['tiempo_Juego_total'].sum().reset_index()
 
-    # Encuentra el usuario con más horas jugadas para el género
+        # Encuentra el usuario en relacion  tiempo de Juego
     max_hours_user = datos_agrupados[datos_agrupados.groupby('user_id')['tiempo_Juego_total'].transform('max') == datos_agrupados['tiempo_Juego_total']]
     if not max_hours_user.empty:
         max_user = max_hours_user.iloc[0]['user_id']
 
-        # Calcula la acumulación de horas jugadas por año
+        # Calculo
         horas_por_anio = datos_agrupados.groupby('Lanzamiento')['tiempo_Juego_total'].sum().reset_index()
         horas_por_anio = horas_por_anio.to_dict(orient='records')
 
@@ -82,25 +82,25 @@ dataframe2 = pd.read_csv('dfx_steam_games.csv')
 @app.get('/usersrecommend/{anio}')
 
 def usersrecommend(anio: int):
-    # Filtra el DataFrame1 para obtener revisiones recomendadas y comentarios positivos/neutrales
+        # Filtro teniendo en cuenta los positivos/
     filtro_df1 = dataframe1[(dataframe1['Recomendacion'] == True)]
     dataframe2['Lanzamiento'] = dataframe2['Lanzamiento'].astype(int)
     
-    # Filtra el DataFrame2 para el año dado
+        # Filtra en relacion al año
     filtro_df2 = dataframe2[dataframe2['Lanzamiento'] == anio]
     
 
     if filtro_df1.empty or filtro_df2.empty:
         return {"Mensaje": "No se encontraron juegos recomendados para el año especificado"}
 
-    # Combina los DataFrames en función de 'Item_id' y cuenta las revisiones recomendadas
+        # Combinacion
     juegos_recomendados = filtro_df1.merge(filtro_df2, left_on='Item_id', right_on='Id')
     top_juegos = juegos_recomendados.groupby('Nombre_del_contenido')['Recomendacion'].count().reset_index()
 
-    # Ordena los juegos por la cantidad de recomendaciones en orden descendente y toma los 3 primeros
+        # Ordena los juegos
     top_juegos = top_juegos.sort_values(by='Recomendacion', ascending=False).head(3)
 
-    # Formatea el resultado en el formato deseado
+        # Resultado
     resultado = [{"Puesto " + str(i + 1): juego} for i, juego in enumerate(top_juegos['Nombre_del_contenido'])]
 
     return resultado
@@ -109,23 +109,23 @@ def usersrecommend(anio: int):
 
 @app.get('/usersnotrecommend/{anio}')
 def usersnotrecommend(anio: int):
-    # Filtra el DataFrame1 para obtener revisiones no recomendadas y comentarios negativos
+        # Filtro teniendo en cuenta los negativos
     filtro_df1 = dataframe1[(dataframe1['Recomendacion'] == False)]
 
-    # Filtra el DataFrame2 para el año dado
+        # Filtro segun año
     filtro_df2 = dataframe2[dataframe2['Lanzamiento'] == anio]
 
     if filtro_df1.empty or filtro_df2.empty:
         return {"Mensaje": "No se encontraron juegos no recomendados para el año especificado"}
 
-    # Combina los DataFrames en función de 'Item_id' y cuenta las revisiones no recomendadas
+    
     juegos_no_recomendados = filtro_df1.merge(filtro_df2, left_on='Item_id', right_on='Id')
     top_juegos = juegos_no_recomendados.groupby('Nombre_del_contenido')['Recomendacion'].count().reset_index()
 
-    # Ordena los juegos por la cantidad de no recomendaciones en orden descendente y toma los 3 primeros
+        # Ordenar
     top_juegos = top_juegos.sort_values(by='Recomendacion', ascending=False).head(3)
 
-    # Formatea el resultado en el formato deseado
+        # Salida
     resultado = [{"Puesto " + str(i + 1): juego} for i, juego in enumerate(top_juegos['Nombre_del_contenido'])]
 
     return resultado
@@ -138,15 +138,92 @@ df = pd.read_csv('dfx_sentimiento.csv')
 
 def get_sentiment_analysis(año: int):
     
-    # Filtrar las reseñas por el año de lanzamiento
+        # Filtro segun lanzamiento
     filtered_df = df[df['Lanzamiento'] == año]
 
-    # Calcular la cantidad de registros por análisis de sentimiento
+        # Calculo la cantidad de registros 
     sentiment_counts = filtered_df['Sentimiento'].value_counts()
 
-    # Convertir el resultado a un diccionario
+        # Salida
     result = {key: value for key, value in zip(sentiment_counts.index, sentiment_counts)}
     
     return result
 
 #///////////////////////////////////////////////////////////////////////////////////////////////////
+df_juegos = pd.read_csv('dfx_merge_sistema.csv')
+
+    # Combinar 
+df_juegos['combined_features'] = df_juegos['Etiquetas'] + ' ' + df_juegos['Especificaciones'] + ' ' + df_juegos['Genero']
+
+    # Asegurar
+df_juegos['combined_features'] = df_juegos['combined_features'].apply(lambda x: ' '.join(x.split()))
+
+    # Matriz
+count_vectorizer = CountVectorizer()
+count_matrix = count_vectorizer.fit_transform(df_juegos['combined_features'])
+
+    # Similitud 
+cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+@app.get('/recomendacion_juego/{Id}')
+
+def recomendacion_juego(Id: int):
+    try:
+        
+        idx = df_juegos[df_juegos['Id'] == Id].index[0]
+
+        
+        sim_scores = list(enumerate(cosine_sim[idx]))
+
+        
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+        
+        top_juegos = sim_scores[1:6]
+
+        
+        recommended_juegos = [df_juegos.iloc[i[0]]['Nombre_del_contenido'] for i in top_juegos]
+
+        return recommended_juegos
+    except IndexError:
+        return {"message": "Juego no encontrado"}
+
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+df_juegos = pd.read_csv('dfx_merge_sistema.csv')
+
+    # Combinar 
+df_juegos['combined_features'] = df_juegos['Etiquetas'] + ' ' + df_juegos['Especificaciones'] + ' ' + df_juegos['Genero']
+
+    # Asegurar
+df_juegos['combined_features'] = df_juegos['combined_features'].apply(lambda x: ' '.join(x.split()))
+
+    # Matriz
+count_vectorizer = CountVectorizer()
+count_matrix = count_vectorizer.fit_transform(df_juegos['combined_features'])
+
+    # Similitud 
+cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+@app.get('/recomendacion_juego/{User_id}')
+
+def recomendacion_juego(User_id: str):
+    try:
+        
+        idx = df_juegos[df_juegos['User_id'] == User_id].index[0]
+
+        
+        sim_scores = list(enumerate(cosine_sim[idx]))
+
+        
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+        
+        top_juegos = sim_scores[1:6]
+
+        
+        recommended_juegos = [df_juegos.iloc[i[0]]['Nombre_del_contenido'] for i in top_juegos]
+
+        return recommended_juegos
+    except IndexError:
+        return {"message": "Juego no encontrado"}
